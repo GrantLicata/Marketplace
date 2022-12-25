60688224e5ec6e0c672425cbe798b2c10cf063be
GrantLicata <grant.licata@gmail.com>
2022-12-24 22:15:01 -0800

    Products, login, and dashboard components built

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import Dashboard from './views/Dashboard';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './components/Login';
+import Navigation from './components/Navigation';
+import ProductList from './components/ProductList';
 
 function App() {
   return (
     <div>
-      <Dashboard />
+      <Navigation/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ProductList/>} />
+          <Route path='/login' element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+
+  ],
 }
