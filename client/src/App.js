91b9b109272121198b51b73d9c0ac3c6f63fd562
GrantLicata <grant.licata@gmail.com>
2022-12-24 19:43:23 -0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-center underline">
+      Welcome to Marketplace!
+    </h1>
+  );
+}
+
+export default App;
