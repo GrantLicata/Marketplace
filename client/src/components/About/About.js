91fbb8e04482960cc7aa20cf112d0144eac80c85
GrantLicata <grant.licata@gmail.com>
2022-12-30 14:05:36 -0800

    About page progress

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -3,9 +3,17 @@ import vegetable_image from './images/vegetable_market.jpg';
 
 const About = () => {
   return (
-    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-      <div className=''>
-        <img src={vegetable_image} alt="market-image" className='h-80'/>
+    <div className='mx-auto max-w-7xl pr-4 sm:pr-6 lg:pr-8'>
+      <div className='sm:grid sm:grid-cols-3'>
+        <div>
+          <img src={vegetable_image} alt="market-image" className='h-84 sr-only sm:not-sr-only'/>
+        </div>
+        <div className='col-span-2 ml-4 mt-2'>
+          <h3 className='font-medium'>About the Marketplace</h3>
+          <p className='mt-1'>Duis est est qui reprehenderit esse deserunt cupidatat reprehenderit velit aute culpa nostrud amet eu. Amet consequat adipisicing cillum non id officia enim cupidatat elit laboris. Laboris laborum qui sint cillum in laborum aliqua ut elit. Veniam exercitation sunt ex est id duis Lorem veniam Lorem do ea eiusmod eu. Esse magna duis deserunt nostrud laboris consequat est. Excepteur mollit consequat eu nulla est esse minim aute enim minim.</p>
+          <p className='mt-1'>Duis est est qui reprehenderit esse deserunt cupidatat reprehenderit velit aute culpa nostrud amet eu. Amet consequat adipisicing cillum non id officia enim cupidatat elit laboris. Laboris laborum qui sint cillum in laborum aliqua ut elit. Veniam exercitation sunt ex est id duis Lorem veniam Lorem do ea eiusmod eu. Esse magna duis deserunt nostrud laboris consequat est. Excepteur mollit consequat eu nulla est esse minim aute enim minim.</p>
+          <p className='mt-1'>Duis est est qui reprehenderit esse deserunt cupidatat reprehenderit velit aute culpa nostrud amet eu. Amet consequat adipisicing cillum non id officia enim cupidatat elit laboris. Laboris laborum qui sint cillum in laborum aliqua ut elit. Veniam exercitation sunt ex est id duis Lorem veniam Lorem do ea eiusmod eu. Esse magna duis deserunt nostrud laboris consequat est. Excepteur mollit consequat eu nulla est esse minim aute enim minim.</p>
+        </div>
       </div>
     </div>
   )
